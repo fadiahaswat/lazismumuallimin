620e49bc69430135d5b56e12a384cb24f98933fd
fadiahaswat <andiaqillahfadiahaswat@gmail.com>
2025-11-27 17:40:05 +0700

    Update data-kelas.js

--- data-kelas.js
+++ data-kelas.js
@@ -5,6 +5,7 @@ const classMetaData = {
     '3B': { wali: 'Ust. Candra', musyrif: 'Ust. Dedi' },
     '4C': { wali: 'Ust. Eko', musyrif: 'Ust. Fajar' },
     '5D': { wali: 'Ust. Gilang', musyrif: 'Ust. Hadi' },
+    '1A': { wali: 'Ust. Kalcer', musyrif: 'Ust. Gus Miftah' },
     
     // Silakan tambahkan kelas lain di bawah ini sesuai kebutuhan
     // Contoh:
