8b0141148387bee0827aa7725ff3057256925c7c
fadiahaswat <andiaqillahfadiahaswat@gmail.com>
2025-12-14 16:53:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2877,37 +2877,6 @@ window.refreshDashboard = async function() {
 }
 
 /* ============================================================================
-   14. FITUR KWITANSI ULTIMATE (IMAGE TEMPLATE SUPPORT)
-   ============================================================================ */
-
-// --- HELPER 1: TERBILANG (Mengubah Angka jadi Kalimat) ---
-function terbilang(angka) {
-    const bil = ["", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
-    angka = parseInt(angka);
-    if (isNaN(angka)) return "";
-    if (angka < 12) return bil[angka];
-    if (angka < 20) return terbilang(angka - 10) + " Belas";
-    if (angka < 100) return terbilang(Math.floor(angka / 10)) + " Puluh " + terbilang(angka % 10);
-    if (angka < 200) return "Seratus " + terbilang(angka - 100);
-    if (angka < 1000) return terbilang(Math.floor(angka / 100)) + " Ratus " + terbilang(angka % 100);
-    if (angka < 2000) return "Seribu " + terbilang(angka - 1000);
-    if (angka < 1000000) return terbilang(Math.floor(angka / 1000)) + " Ribu " + terbilang(angka % 1000);
-    if (angka < 1000000000) return terbilang(Math.floor(angka / 1000000)) + " Juta " + terbilang(angka % 1000000);
-    return "";
-}
-
-// --- HELPER 2: LOAD IMAGE (Untuk memuat background MasterTemplate.png) ---
-function loadImage(url) {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.crossOrigin = "Anonymous"; 
-        img.src = url;
-        img.onload = () => resolve(img);
-        img.onerror = (e) => reject(e);
-    });
-}
-
-/* ============================================================================
    14. FITUR KWITANSI ULTIMATE (MODAL PREVIEW + PDF PRESISI)
    ============================================================================ */
 
